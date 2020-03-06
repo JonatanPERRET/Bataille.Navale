ceb48e25cdb44bb17aa2ca2b6a47ead985d78e51
Jonatan Perret <jonatan.perret@cpnv.ch>
2020-03-06 22:09:34 +0100

    resolution bug mdp

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <windows.h>
 #include <string.h>
 
-
 void creerUnCompte (){
     char UserName[48];
     char Name[48];
@@ -25,13 +24,15 @@ void creerUnCompte (){
     fprintf(User, "%s\n%s\n%s\n%s", UserName, Name, Prenom, MDP);
     fclose(User);
 }
-char seConnecter(){
+void seConnecter(){
     char UserName[48] = {0};
     char Name[48] = {0};
     char Prenom[48] = {0};
     char MDP[48] = {0};
     char MDPVal[48] = {0};
-
+    int VerifMDP = 0;
+    int Choix = 0;
+    system("cls");
     printf("Nom d'utilisateur :\n");
     scanf("%s", &UserName);
     FILE *User = NULL;
@@ -42,20 +43,33 @@ char seConnecter(){
         fscanf(User, "%s", Prenom);
         fscanf(User, "%s", MDP);
         fclose(User);
+        system("cls");
         printf("Mot de passe :\n");
         scanf("%s", MDPVal);
-        if (MDPVal == MDP){
+        for (int i = 0; i < 48; i++) {
+            if (MDPVal[i] == MDP[i]) {
+                VerifMDP++;
+            }
+        }
+        if (VerifMDP == 48){
             printf("JUSTE");
         }
-        else printf("MDP incorect\n");
+        else printf("Mot de passe incorect\n");
         system("Pause");
     }
-    else printf("Utilisateur non trouve\n");
+    else {
+        printf("Utilisateur non trouve\nVoulez vous creer un compte ?");
+        scanf("%d", &Choix);
+        if (Choix == 1) creerUnCompte();
+        else printf("Choix incorect");
+    }
     system("Pause");
 }
 
+
 int main() {
 
+    char GlobalUser[48] = {0};
     int Choix = 0;
     system("cls");
     printf("Bonjour,\nBienvenue dans le jeu de la bataille navale.\nChoisissez une option :");
@@ -65,6 +79,7 @@ int main() {
         case 1:
             printf("Entrez vos identifiants :");
             seConnecter();
+            printf("%c", GlobalUser);
             break;
         case 2:
             system("cls");
