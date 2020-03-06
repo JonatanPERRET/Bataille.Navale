812b1199b10d77e1667a5aa9bf7508b1559b63ea
PERRET Jonatan <Jonatan.PERRET@cpnv.ch>
2020-03-06 14:50:54 +0100

    Ajout de la fonction se connecter

--- main.c
+++ main.c
@@ -3,12 +3,16 @@
 #include <windows.h>
 #include <string.h>
 
+
 void creerUnCompte (){
+    char UserName[48];
     char Name[48];
     char Prenom[48];
     char MDP[48];
 
     printf("Creer un compte, Entrez vos identifiants :\n");
+    printf("Nom d'utilisateur :\n");
+    scanf("%s", UserName);
     printf("Nom :\n");
     scanf("%s", &Name);
     printf("Prenom :\n");
@@ -17,11 +21,38 @@ void creerUnCompte (){
     scanf("%s", &MDP);
 
     FILE *User = NULL;
-    User = fopen(Name, "w");
-    fprintf(User, "%s\n%s\n%s", Name, Prenom, MDP);
+    User = fopen(UserName, "w");
+    fprintf(User, "%s\n%s\n%s\n%s", UserName, Name, Prenom, MDP);
     fclose(User);
 }
+char seConnecter(){
+    char UserName[48] = {0};
+    char Name[48] = {0};
+    char Prenom[48] = {0};
+    char MDP[48] = {0};
+    char MDPVal[48] = {0};
 
+    printf("Nom d'utilisateur :\n");
+    scanf("%s", &UserName);
+    FILE *User = NULL;
+    User = fopen(UserName, "r+");
+    if (User != NULL) {
+        fscanf(User, "%s", UserName);
+        fscanf(User, "%s", Name);
+        fscanf(User, "%s", Prenom);
+        fscanf(User, "%s", MDP);
+        fclose(User);
+        printf("Mot de passe :\n");
+        scanf("%s", MDPVal);
+        if (MDPVal == MDP){
+            printf("JUSTE");
+        }
+        else printf("MDP incorect\n");
+        system("Pause");
+    }
+    else printf("Utilisateur non trouve\n");
+    system("Pause");
+}
 
 int main() {
 
@@ -33,6 +64,7 @@ int main() {
     switch (Choix){
         case 1:
             printf("Entrez vos identifiants :");
+            seConnecter();
             break;
         case 2:
             system("cls");
