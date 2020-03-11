6fa2cbe3916e512c6f557d1151f207e484417802
PERRET Jonatan <Jonatan.PERRET@cpnv.ch>
2020-03-11 11:53:10 +0100

    ajout de la fonction Jouer une partie

--- main.c
+++ main.c
@@ -79,6 +79,48 @@ void seConnecter(){
         }
     }
 }
+void jouerPartie (){
+    int compteurJoueur = 0;
+    int Vertical = 0;
+    int Horizontal = 0;
+    int TableauJeu [10][10] = {0};
+    int TableauJeu1 [10][10] = {0,0,0,0,0,0,1,0,0,0,1,1,1,1,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
+    int TableauJeu2 [10][10] = {0};
+    int TableauJeu3 [10][10] = {0};
+    int TableauJeu4 [10][10] = {0};
+    int TableauJeu5 [10][10] = {0};
+
+    do {
+        system("cls");
+        printf("Voici le plateau :\n");
+        printf("   1 2 3 4 5 6 7 8 9 10\n");
+        for (int i = 0; i < 10; i++) {
+            printf("%2d ", i+1);
+            for (int j = 0; j < 10; j++) {
+                if (TableauJeu1[i][j]==0) printf("%c ", 219);
+                if (TableauJeu1[i][j]==1) printf("%c ", 219);
+                if (TableauJeu1[i][j]==2) printf("\033[34m%c \033[0m", 219);
+                if (TableauJeu1[i][j]==3) printf("\033[31m%c \033[0m", 219);
+                //printf("%d ", TableauJeu[i][j]);
+            }
+            printf("\n");
+        }
+        printf("Ou voulez vous tirer ?\nVertical :\n");
+        scanf("%d", &Vertical);
+        printf("Horizontal :\n");
+        scanf("%d", &Horizontal);
+        compteurJoueur += 1;
+        TableauJeu1[Vertical - 1][Horizontal - 1] += 2;
+    }
+    while (Vertical < 11);
+
+
+
+    system("Pause");
+
+
+
+}
 
 void menuPrincipal(){
     int Choix = 0;
@@ -89,23 +131,30 @@ void menuPrincipal(){
     switch (Choix){
         case 1: {
             system("cls");
-            printf("Jouer une partie\n");
+            jouerPartie();
             break;
         }
         case 2:{
+            do {
+                system("cls");
+                printf("La bataille navale oppose deux joueurs qui s'affrontent. \nChacun a une flotte composee de 5 bateaux, qui sont les suivants : \n1 porte-avion (5 cases), 1 croiseur (4 cases), 1 contre-torpilleur (3 cases), \n1 sous-marin (3 cases), 1 torpilleur (2 cases). \nLes bateaux ne doivent pas etre colles entre eux.\n Le but du jeux est d'abbatre tout les bateaux enemis");
+                printf("\n\nAppuyer sur 1 pour revenir au menu principale\n");
+                scanf("%d", &Choix);
+                if (Choix == 1) menuPrincipal();
+            }
+            while (Choix != 1);
+        }
+        default:
+        {
             system("cls");
-            printf("La bataille navale oppose deux joueurs qui s'affrontent. \nChacun a une flotte composee de 5 bateaux, qui sont, en general, les suivants : \n1 porte-avion (5 cases), 1 croiseur (4 cases), 1 contre-torpilleur (3 cases), \n1 sous-marin (3 cases), 1 torpilleur (2 cases). \nLes bateaux ne doivent pas etre colles entre eux.\n Le but du jeux est d'abbatre tout les bateaux enemis");
-            printf("\nAppuyer sur 1 pour revenir au menu principale\n");
-            scanf("%d", &Choix);
-            if (Choix == 1) menuPrincipal();
+            menuPrincipal();
         }
     }
 
 }
 
-int main() {
 
-    char GlobalUser[48] = {0};
+int main() {
     int Choix = 0;
     system("cls");
     printf("Bonjour,\nBienvenue dans le jeu de la bataille navale.\nChoisissez une option :");
